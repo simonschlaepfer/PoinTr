fold fix and wrong scaling fix

datasets/data_transforms.py
@@ -38,7 +38,7 @@ class Compose(object):
                         ]:
                             data[k] = transform(v, rnd_value)
                         elif transform.__class__ in [
-                            RandomRotatePoints
+                            RandomRotatePoints, RandomShiftCloud
                         ]:
                             data[k] = transform(v, rnd_value, rnd_axis)
                         else:
@@ -157,6 +157,16 @@ class RandomShiftPoints(object):
         shift = np.random.normal(0, self.shift_range, ptcloud.shape)
         ptcloud += shift
         return ptcloud
+
+class RandomShiftCloud(object):
+    def __init__(self, parameters) -> None:
+        self.shift_range = parameters['shift_range']
+
+    def __call__(self, ptcloud, rnd_value, rnd_axis):
+        shift = self.shift_range * rnd_axis
+        ptcloud += shift
+        return ptcloud
+
 
 class NormalizeObjectPose(object):
     def __init__(self, parameters):

models/AdaPoinTr.py
@@ -752,9 +752,11 @@ class HandFold(nn.Module):
 
         x = torch.cat([seed, features], dim=1) # b*mx386x64
         fd1 = self.folding1(x)
+        # print("simon forward", x.shape, fd1.shape)
         x = torch.cat([fd1, features], dim=1)
         fd2 = self.folding2(x) # b*mx3x64
         # print("simon conv shape", fd2.shape)
+        # print("simon forward", x.shape, fd2.shape)
 
         return fd2
 
@@ -793,7 +795,7 @@ class Fold(nn.Module):
     def forward(self, x):
         num_sample = self.step * self.step
         bs = x.size(0)
-        print("fold debug", x.shape, bs)
+        # print("fold debug", x.shape, bs)
         features = x.view(bs, self.in_channel, 1).expand(bs, self.in_channel, num_sample) # b*mx384x64
         seed = self.folding_seed.view(1, 2, num_sample).expand(bs, 2, num_sample).to(x.device) # b*mx2x64
         # print("simon input shape", x.shape, num_sample, features.shape, seed.shape)
@@ -978,12 +980,10 @@ class AdaPoinTr(nn.Module):
             self.factor = self.fold_step**2
             self.decode_head = Fold(self.trans_dim, step=self.fold_step, hidden_dim=256)  # rebuild a cluster point
             # print("debug linear layer", self.num_query * self.factor, self.num_query * self.factor // 2)
-            self.reduce_layer = nn.Linear(self.num_query * self.factor, self.num_query * self.factor // 2)
         elif self.decoder_type == 'hand_fold':
             self.factor = self.fold_step**2
             self.decode_head = HandFold(self.trans_dim, step=self.fold_step, hidden_dim=256)  # rebuild a cluster point
             # print("debug linear layer", self.num_query * self.factor, self.num_query * self.factor // 2)
-            self.reduce_layer = nn.Linear(self.num_query * self.factor, self.num_points)
         else:
             if self.num_points is not None:
                 self.factor = self.num_points // self.num_query
@@ -1057,9 +1057,7 @@ class AdaPoinTr(nn.Module):
             pred_fine = rebuild_points[:, :-denoise_length].reshape(B, -1, 3).contiguous()
             # print("simon intermediate", pred_fine.shape)
             if self.decoder_type == 'fold' or self.decoder_type == 'hand_fold':
-                pred_fine_t = pred_fine.transpose(1,2).contiguous()
-                pred_fine_t = self.reduce_layer(pred_fine_t)
-                pred_fine = pred_fine_t.transpose(1,2).contiguous()
+                # print("simon pred fine size", pred_fine.shape, self.num_points)
                 assert pred_fine.size(1) == self.num_points
             else:
                 # print("simon here")
@@ -1079,9 +1077,6 @@ class AdaPoinTr(nn.Module):
             assert denoise_length == 0
             rebuild_points = rebuild_points.reshape(B, -1, 3).contiguous()  # B N 3
             if self.decoder_type == 'fold' or self.decoder_type == 'hand_fold':
-                rebuild_points_t = rebuild_points.transpose(1,2).contiguous() 
-                rebuild_points_t = self.reduce_layer(rebuild_points_t)
-                rebuild_points = rebuild_points_t.transpose(1,2).contiguous()
                 assert rebuild_points.size(1) == self.num_points
             else:
                 assert rebuild_points.size(1) == self.num_query * self.factor

tools/inference.py
@@ -10,6 +10,7 @@ import cv2
 import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(BASE_DIR, '../'))
+from tqdm import tqdm
 
 from builder import *
 from utils.config import cfg_from_yaml_file
@@ -59,18 +60,18 @@ def inference_single(model, pc_path, args, config, root=None):
     # read single point cloud
     pc_ndarray = IO.get(pc_file).astype(np.float32)
     # transform it according to the model 
-    # if config.dataset.train._base_['NAME'] == 'ShapeNet':
+    if config.dataset.train._base_['NAME'] == 'ShapeNet':
         # normalize it to fit the model on ShapeNet-55/34
-    print("input data is normalized")
-    centroid = np.mean(pc_ndarray, axis=0)
-    pc_ndarray = pc_ndarray - centroid
-    m = np.max(np.sqrt(np.sum(pc_ndarray**2, axis=1)))
-    pc_ndarray = pc_ndarray / m
+        print("input data is normalized")
+        centroid = np.mean(pc_ndarray, axis=0)
+        pc_ndarray = pc_ndarray - centroid
+        m = np.max(np.sqrt(np.sum(pc_ndarray**2, axis=1)))
+        pc_ndarray = pc_ndarray / m
 
     transform = Compose([{
         'callback': 'UpSamplePoints',
         'parameters': {
-            'n_points': 4096
+            'n_points': 2048
         },
         'objects': ['input']
     }, {
@@ -115,7 +116,7 @@ def main():
 
     if args.pc_root != '':
         pc_file_list = os.listdir(args.pc_root)
-        for pc_file in pc_file_list:
+        for pc_file in tqdm(pc_file_list):
             inference_single(base_model, pc_file, args, config, root=args.pc_root)
     else:
         inference_single(base_model, args.pc, args, config)

tools/runner.py
@@ -17,7 +17,6 @@ def run_net(args, config, train_writer=None, val_writer=None):
     (train_sampler, train_dataloader), (_, test_dataloader) = builder.dataset_builder(args, config.dataset.train), \
                                                             builder.dataset_builder(args, config.dataset.val)
     
-    print("simon debug", len(test_dataloader))
     # build model
     base_model = builder.model_builder(config.model)
     if args.use_gpu:
@@ -102,8 +101,8 @@ def run_net(args, config, train_writer=None, val_writer=None):
                 # partial = data[0][0].detach().cpu().numpy()
                 # gt = data[1][0].detach().cpu().numpy()
                 # print(partial, partial.shape, gt.shape)
-                # np.save(os.path.join('/cluster/scratch/simschla/PoinTr/debug', 'input2.npy'), partial)
-                # np.save(os.path.join('/cluster/scratch/simschla/PoinTr/debug', 'gt2.npy'), gt)
+                # np.save(os.path.join('/cluster/scratch/simschla/PoinTr/debug', 'input6.npy'), partial)
+                # np.save(os.path.join('/cluster/scratch/simschla/PoinTr/debug', 'gt6.npy'), gt)
                 partial = data[0].cuda()
                 gt = data[1].cuda()
                 if config.dataset.train._base_.CARS: